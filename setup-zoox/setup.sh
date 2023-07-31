a8d0a6c7976e457cc215ddce52f6c482d51fcad2
Christopher Patton <chris@zoox.com>
2023-07-31 16:29:38 -0700

    build git, tmux, and vim from source

--- setup-zoox/setup.sh
+++ setup-zoox/setup.sh
@@ -16,15 +16,13 @@ sudo apt-get install --assume-yes \
   "linux-headers-$(uname -r)" \
   "linux-image-$(uname -r)"
 
-# Pi-Rho PPA
-sudo apt-add-repository --yes ppa:pi-rho/dev
-sudo apt-get update
-
 # User tools
 sudo apt-get install --assume-yes \
   build-essential \
+  cmake \
   curl \
-  tmux=2.0-1~ppa1~t \
+  git \
+  tmux \
   tree \
   wget \
   vagrant \
@@ -48,3 +46,37 @@ sudo apt-get install --assume-yes \
   virtualbox \
   virtualbox-dkms \
   virtualbox-guest-additions-iso
+
+# Tmux build dependencies
+sudo apt-get install --assume-yes \
+  libncurses5-dev \
+  libncursesw5-dev \
+  libevent-dev
+
+# Git build dependencies
+sudo apt-get install --assume-yes \
+  asciidoc \
+  dh-autoreconf \
+  docbook2x \
+  gettext \
+  install-info \
+  libcurl4-gnutls-dev \
+  libexpat1-dev \
+  libssl-dev \
+  libz-dev \
+  xmlto
+
+# Vim build dependencies
+sudo apt-get install --assume-yes \
+  libatk1.0-dev \
+  libcairo2-dev \
+  libgtk2.0-dev \
+  liblua5.1-0-dev \
+  libncurses5-dev \
+  libperl-dev \
+  libx11-dev \
+  libxpm-dev \
+  libxt-dev \
+  lua5.1 \
+  python3-dev \
+  ruby-dev
