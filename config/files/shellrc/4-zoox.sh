bce3287090d91ba73fff424de6d0dc7ee308f79e
Christopher Patton <chpatton013@gmail.com>
2023-08-01 13:06:58 -0700

    Fix all issues with config sync

--- config/files/shellrc/4-zoox.sh
+++ config/files/shellrc/4-zoox.sh
@@ -47,3 +47,12 @@ function coowners() {
   git grep -l "$@" -- '*OWNERS' |
     xargs -I{} sh -c 'echo \#\#\# {}; cat {}; echo'
 }
+
+function dir-ccb-somepath() {
+  dirpath="$1"
+  shift
+  universe="//$dirpath"
+  files="$(find "$dirpath" -type f | tr '[[:space:]]' ' ')"
+  targets="$(bazel query --keep_going --universe_scope="$universe" --order_output=no "kind(rule, allrdeps(set($files), 1))" 2>/dev/null | tr '[[:space:]]' ' ')"
+  bazel query "$@" "somepath(//vehicle:l3_ccb_regulated_package, set($targets))"
+}
